ff1361c70a5f67c771f567ee9ccead9f54e69044
bohorquezbs <155646973+bohorquezbs@users.noreply.github.com>
2024-10-01 19:21:59 -0500

    proceso

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        '900':'32rem'
+      }
+    },
   },
   plugins: [],
 }
